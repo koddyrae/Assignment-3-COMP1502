635ce00a6664ea6936d29dfe7ae3eab11cf3319b
Koddy Rae Madriaga <koddyrae@gmail.com>
2023-04-04 23:01:32 -0600

    controller ids
    added ids to the fields and buttons on the gui

--- toy-store-gui/src/ca/cyberscientist/toystoredb/controller/SceneController.java
+++ toy-store-gui/src/ca/cyberscientist/toystoredb/controller/SceneController.java
@@ -1,5 +1,103 @@
 package ca.cyberscientist.toystoredb.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+
 public class SceneController {
+	@FXML
+	private TextField addAgeAppropriate;
+	@FXML
+	private TextField addAvailableCount;
+	@FXML
+	private TextField addBrand;
+	@FXML
+	private TextField addClassification;
+	@FXML
+	private TextField addDesigners;
+	@FXML
+	private TextField addMaterial;
+	@FXML
+	private TextField addMaxNumPlayers;
+	@FXML
+	private TextField addMinNumPlayers;
+	@FXML
+	private TextField addName;
+	@FXML
+	private TextField addPrice;
+	@FXML
+	private TextField addPuzzleType;
+	@FXML
+	private TextField addSerialNum;
+	@FXML
+	private TextField addSize;
+	@FXML
+	private Tab addToyTab;
+	@FXML
+	private Button buyButton;
+	@FXML
+	private Button clearButton;
+	@FXML
+	private Tab homeTab;
+	@FXML
+	private ListView<?> listView;
+	@FXML
+	private RadioButton radioName;
+	@FXML
+	private RadioButton radioSerialNum;
+	@FXML
+	private RadioButton radioType;
+	@FXML
+	private Button removeButton;
+	@FXML
+	private TextField removeSerialNumber;
+	@FXML
+	private Tab removeToyTab;
+	@FXML
+	private Button saveButton;
+	@FXML
+	private Button searchButton;
+	@FXML
+	private TextField selectName;
+	@FXML
+	private TextField selectSerialNum;
+	@FXML
+	private TextField selectType;
+	@FXML
+	private MenuButton toyTypeMenu;
 	
+	@FXML
+	void buyButtonHome(ActionEvent event) {
+
+	}
+
+	@FXML
+	void clearButtonHome(ActionEvent event) {
+
+	}
+
+	@FXML
+	void removeToyBtn(ActionEvent event) {
+
+	}
+
+	@FXML
+	void saveFieldsButton(ActionEvent event) {
+
+	}
+
+	@FXML
+	void searchButtonHome(ActionEvent event) {
+
+	}
+
+	@FXML
+	void typeMenu(ActionEvent event) {
+
+	}
 }
